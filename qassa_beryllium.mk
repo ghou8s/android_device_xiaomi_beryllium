b709d2d9a72d5b006884f019887f8ba83b2129bc
ghou8s <ghou8s@gmail.com>
2023-10-04 16:47:29 +0000

    beryllium: optout QTI Perf

--- qassa_beryllium.mk
+++ qassa_beryllium.mk
@@ -12,6 +12,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
+QTI_OPTOUT := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := qassa_beryllium
